9420fd0527a57ef8a0a8f37b7f4af5d8217e7f35
Winilson de Paula <wpaula@alfapeople.com.br>
2021-03-02 19:06:07 -0300

    add js

---
+++ Principal.Lab/Customization/JScript/contact.js
@@ -0,0 +1,29 @@
+﻿function ValidarMetodoPreferencialdeComunicacao(contexto) {
+
+    var ContextoCrm = contexto.getFormContext();
+    
+    var prefferedContactMethodCode =
+        ContextoCrm.getAttribute('preferredcontactmethodcode').getValue();
+
+    if (prefferedContactMethodCode == 1) {
+        clearAllMandatoryFields(contexto);
+    }
+    if (prefferedContactMethodCode == 2) {
+        clearAllMandatoryFields(contexto);
+        ContextoCrm.getAttribute('emailaddress1').setRequiredLevel('required');
+    } else if (prefferedContactMethodCode == 3) {
+        clearAllMandatoryFields(contexto);
+        ContextoCrm.getAttribute('mobilephone').setRequiredLevel('required');
+    } else if (prefferedContactMethodCode == 4) {
+        clearAllMandatoryFields(contexto);
+        ContextoCrm.getAttribute('fax').setRequiredLevel('required');
+    }
+}
+function clearAllMandatoryFields(contexto) {
+
+    var contextoCrm = contexto.getFormContext();
+   
+    contextoCrm.getAttribute('emailaddress1').setRequiredLevel('none');
+    contextoCrm.getAttribute('mobilephone').setRequiredLevel('none');
+    contextoCrm.getAttribute('fax').setRequiredLevel('none');
+}
